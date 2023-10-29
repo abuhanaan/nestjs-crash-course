209de9d9a77c9897f1238ad41709141405bfbc77
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-10-29 14:51:07 +0100

    Serviices

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -2,10 +2,12 @@ import {
   Body,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
-  Query,
+  //   Query,
   Req,
   Res,
   UsePipes,
@@ -13,13 +15,16 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get()
-  getUsers(@Query('sortBy') sortBy: string) {
-    console.log(sortBy);
-    return [{ username: 'ade', email: 'ade@email.com' }];
+  //   getUsers(@Query('sortBy') sortBy: string) {
+  getUsers() {
+    return this.usersService.fetchUsers();
   }
 
   @Get('posts')
@@ -55,16 +60,25 @@ export class UsersController {
   @UsePipes(new ValidationPipe())
   createUser(@Body() userData: CreateUserDto) {
     console.log(userData);
-    return {};
+    return this.usersService.createUser(userData);
+    // return {};
   }
 
   // Working with url parameter
-  @Get(':id/:postId')
-  getUserById(
-    @Param('id', ParseIntPipe) id: number,
-    @Param('postId') postId: string,
-  ) {
-    console.log({ id, postId });
-    return { id, postId };
+  //   @Get(':id/:postId')
+  //   getUserById(
+  //     @Param('id', ParseIntPipe) id: number,
+  //     @Param('postId') postId: string,
+  //   ) {
+  //     console.log({ id, postId });
+  //     return { id, postId };
+  //   }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    const user = this.usersService.fetchUserById(id);
+    if (!user)
+      throw new HttpException('User Not Found', HttpStatus.BAD_REQUEST);
+    return user;
   }
 }
