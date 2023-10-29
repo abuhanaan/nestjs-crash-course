915ce8c06875b7bc463912c3329158b8e97a54d3
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-10-29 14:50:17 +0100

    Serviices

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserType } from 'src/utils/CreateUserType';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private fakeUsers = [
+    { username: 'ade', email: 'ade@email.com' },
+    { username: 'mustopha', email: 'mustopha@email.com' },
+    { username: 'ayo', email: 'ayo@email.com' },
+  ];
+
+  fetchUsers() {
+    return this.fakeUsers;
+  }
+
+  createUser(userData: CreateUserType) {
+    this.fakeUsers.push(userData);
+    return this.fakeUsers;
+  }
+
+  fetchUserById(id: number) {
+    if (id === 321) return { id: 321, username: 'ade', email: 'ade@email.com' };
+  }
+}
